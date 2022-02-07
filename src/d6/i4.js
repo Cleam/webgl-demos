e2ec049e51bf43dcfd0f9cefe4741cf227a64689
lizhigao <lizhigao@021.com>
2022-02-07 18:46:55 +0800

    feat: draw circle

--- src/d6/i4.js
+++ src/d6/i4.js
@@ -15,28 +15,24 @@ const a_Color = gl.getAttribLocation(program, 'a_Color');
 gl.enableVertexAttribArray(a_Position);
 gl.enableVertexAttribArray(a_Color);
 
-// 绘制顶点固定的矩形
+// 绘制顶点固定的圆形
 
-// 三角带，顶点数组
-// prettier-ignore
-// const positions = [
-//   30, 300, 0, 255, 0, 1,   //V0
-//   300, 300, 255, 0, 255, 1,  //V1
-//   30, 30, 255, 255, 0, 1,    //V2
-//   300, 30, 0, 255, 0, 1    //V3
-// ]
+// 生成园顶点的函数
+// x：圆心的 x 坐标
+// y：圆心的 y 坐标
+// radius：半径
+// n：三角形的数量
+function createCircleVertex(x, y, radius, n) {
+  const positions = [x, y, 255, 255, 0, 1];
+  for (let i = 0; i <= n; i++) {
+    const angle = (i * Math.PI * 2) / n;
+    positions.push(x + radius * Math.sin(angle), y + radius * Math.cos(angle), 255, 0, 0, 1);
+  }
+  return positions;
+}
 
-// 三角扇，顶点数组
-// prettier-ignore
-const positions = [
-  165, 165, 255, 255, 0, 1, //V0
-  30, 30, 255, 0, 0, 1,    //V1
-  30, 300, 0, 0, 255, 1,   //V2
-  300, 300, 255, 0, 255, 1,  //V3
-  300, 30, 0, 255, 0, 1,   //V4
-  30, 30, 255, 0, 0, 1,    //V1
-]
-
+const positions = createCircleVertex(200, 200, 150, 15);
+console.log('positions :>> ', positions);
 const buffer = gl.createBuffer();
 gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
 // 设置 a_Position a_Color 属性从缓冲区读取数据方式
@@ -49,6 +45,3 @@ gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
 // gl.drawArrays(gl.TRIANGLE_STRIP, 0, positions.length / 6);
 // 三角扇
 gl.drawArrays(gl.TRIANGLE_FAN, 0, positions.length / 6);
-
-// gl.enable(gl.CULL_FACE);
-// gl.cullFace(gl.FRONT);
